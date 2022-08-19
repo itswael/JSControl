c2c15bdd04b645b3421a0d4e8a915d771df83e02
Mohd Wael <Mohd.wael.cs.2016@miet.ac.in>
2022-08-19 21:09:39 +0530

    added functionality to display data by adding list item while traversing the predefined object on clicking on a button

--- controller..js
+++ controller..js
@@ -25,3 +25,50 @@ function highlightLink(event){
 
 const highButton = document.querySelector("#highlight-links button");
 highButton.addEventListener("click", highlightLink);
+
+//               Third Part
+const listElement1 = document.getElementById("output-user-data");
+const userbtn = document.querySelector("#user-data button");
+let dummyData = {
+    Name : "Wael",
+    age : 25,
+    Qualification : "Graduate",
+}
+
+function displayData(event){
+    listElement1.innerHTML = "";
+    for (const key in dummyData) {
+        let newElement = document.createElement("li");
+        let content = key.toUpperCase()+" : "+dummyData[key];
+        newElement.textContent = content;
+        listElement1.append(newElement);
+    }
+}
+
+userbtn.addEventListener("click",displayData);
+
+
+//               Fourth Part
+
+const diceInput = document.getElementById("user-target-number");
+const diceButton = document.querySelector("#statistics button");
+const x = document.getElementById("output-total-rolls");
+const y = document.getElementById("output-target-number");
+const listElement = document.getElementById("dice-rolls");
+const userInp = diceInput.value;
+function calculateTurns(event){
+    let turns = 1;
+    let num = Math.floor((Math.random() * 6) + 1);
+    x.textContent = turns;
+    y.textContent = userInp;
+    while(userInp != num){
+        let newElement = document.createElement("li");
+        num = Math.floor((Math.random() * 6) + 1);
+        newElement.textContent = num;
+        turns++;
+        x.textContent = turns;
+        listElement.append(newElement);
+    } 
+}
+
+diceButton.addEventListener("click",calculateTurns);
